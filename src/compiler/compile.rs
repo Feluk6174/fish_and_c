1515f0a731e20b05ff521464dba25079349f63cf
Feluk6174 <feluk6174@proton.me>
2024-04-24 11:59:28 +0200

    added base to the asm file

--- src/compiler/compile.rs
+++ src/compiler/compile.rs
@@ -1,9 +1,45 @@
-use std::fs::File;
+use std::{fs::File, io::Write};
 use crate::precompile::branch::Branch;
 use super::functions::{Function, build_functions};
 
 pub fn compile(tree: Vec<Branch>, file_name: &str) -> Result<(), String> {
     let mut functions:Vec<Function> = build_functions(&tree)?;
     let mut file = File::create(file_name).expect("Error writing to file");
+    add_base(file, 10000, 100);
     Ok(())
+}
+
+
+fn add_base(mut file:File, mem_size:u64, p_buf_size:u64) {
+    file.write_all(format!("; Coded in Fish&C
+; https://github.com/Feluk6174/yeso-compiler
+global _start
+
+section .data
+    mem: times {} db 0
+    alloc: times 100 dd 0
+    mem_fi dd 0
+    p_buf_ptr: dq 0
+    p_buf: times {} db 0
+
+section .text
+
+panic_program:
+    mov rbx, QWORD[p_buf_ptr]
+    call print_buffer
+
+    mov rax, 60
+    mov rdi, 120
+    syscall
+
+_start:
+    call main
+    ;end execution
+    
+    mov rbx, QWORD[p_buf_ptr]
+    call print_buffer
+
+    mov rax, 60
+    mov rdi, 0
+    syscall", mem_size, p_buf_size).as_bytes()).expect("Error writing to file");
 }
